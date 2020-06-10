4a5b47f68cab60513afd76b4a8976e67349db1b8
lxxxvi <lxxxvi@users.noreply.github.com>
2020-06-10 11:10:05 +0200

    simplify tests

--- test/services/srf/api/songlog/artist_test.rb
+++ test/services/srf/api/songlog/artist_test.rb
@@ -2,14 +2,14 @@ require 'test_helper'
 
 class Srf::Api::Songlog::ArtistTest < ActiveSupport::TestCase
   test '.new' do
-    Srf::Api::Songlog::Artist.new(srf_api_songlog_artist).tap do |srf_api_artist|
+    srf_api_songlog_artist.tap do |srf_api_artist|
       assert_equal 'Thurston Moore', srf_api_artist.name
       assert_equal 'ARTIST-ID', srf_api_artist.id
     end
   end
 
   test '#to_artist' do
-    Srf::Api::Songlog::Artist.new(srf_api_songlog_artist).to_artist.tap do |artist|
+    srf_api_songlog_artist.to_artist.tap do |artist|
       assert_equal Artist, artist.class
       assert_equal 'Thurston Moore', artist.name
       assert artist.new_record?
@@ -19,11 +19,13 @@ class Srf::Api::Songlog::ArtistTest < ActiveSupport::TestCase
   private
 
   def srf_api_songlog_artist
-    {
-      "name" => "Thurston Moore",
-      "id" => "ARTIST-ID",
-      "modifiedDate" => "2013-02-14T15:50:26+01:00",
-      "createdDate" => "2011-03-24T22:48:14+01:00"
-    }
+    Srf::Api::Songlog::Artist.new(
+      {
+        "name" => "Thurston Moore",
+        "id" => "ARTIST-ID",
+        "modifiedDate" => "2013-02-14T15:50:26+01:00",
+        "createdDate" => "2011-03-24T22:48:14+01:00"
+      }
+    )
   end
 end
