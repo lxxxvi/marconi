2b727b237f2ed603dfd0101e47f5d34708ecce2e
lxxxvi <lxxxvi@users.noreply.github.com>
2020-06-10 11:09:46 +0200

    rename srf_api_artist to srf_api_songlog_artist

--- test/models/artist/finders/srf_test.rb
+++ test/models/artist/finders/srf_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class Artist::Finders::SrfTest < ActiveSupport::TestCase
   test '#find_or_initialize_by for existing artist, with existing external key' do
-    Artist::Finders::Srf.find_or_initialize_by(srf_api_artist_with_existing_external_key) do |artist|
+    Artist::Finders::Srf.find_or_initialize_by(srf_api_songlog_artist_with_existing_external_key) do |artist|
       assert_equal artists(:beatles), artist
     end
   end
 
   test '#find_or_initialize_by for existing artist, without existing external key' do
-    Artist::Finders::Srf.find_or_initialize_by(srf_api_artist_without_existing_external_key).tap do |artist|
+    Artist::Finders::Srf.find_or_initialize_by(srf_api_songlog_artist_without_existing_external_key).tap do |artist|
       assert_equal artists(:pointer_sisters), artist
       assert artist.external_keys.size.positive?
       assert_equal 'POINTER-SISTERS', artist.external_keys.first.identifier
@@ -16,7 +16,7 @@ class Artist::Finders::SrfTest < ActiveSupport::TestCase
   end
 
   test '#find_or_initialize_by for new artist' do
-    Artist::Finders::Srf.find_or_initialize_by(srf_api_artist_new_artist).tap do |artist|
+    Artist::Finders::Srf.find_or_initialize_by(srf_api_songlog_artist_new_artist).tap do |artist|
       assert artist.new_record?
       assert 'Run The Jewels', artist.name
       assert artist.external_keys.size.positive?
@@ -26,15 +26,15 @@ class Artist::Finders::SrfTest < ActiveSupport::TestCase
 
   private
 
-  def srf_api_artist_with_existing_external_key
-    Srf::Api::Artist.new({ 'name' => 'The Beatles', 'id' => 'BEATLES' })
+  def srf_api_songlog_artist_with_existing_external_key
+    Srf::Api::Songlog::Artist.new({ 'name' => 'The Beatles', 'id' => 'BEATLES' })
   end
 
-  def srf_api_artist_without_existing_external_key
-    Srf::Api::Artist.new({ 'name' => 'Pointer Sisters', 'id' => 'POINTER-SISTERS' })
+  def srf_api_songlog_artist_without_existing_external_key
+    Srf::Api::Songlog::Artist.new({ 'name' => 'Pointer Sisters', 'id' => 'POINTER-SISTERS' })
   end
 
-  def srf_api_artist_new_artist
-    Srf::Api::Artist.new({ 'name' => 'Run The Jewels', 'id' => 'RUN-THE-JEWELS' })
+  def srf_api_songlog_artist_new_artist
+    Srf::Api::Songlog::Artist.new({ 'name' => 'Run The Jewels', 'id' => 'RUN-THE-JEWELS' })
   end
 end
