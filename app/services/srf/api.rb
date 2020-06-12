f2ab72d3e641486af086338b3c5375028aca3d38
lxxxvi <lxxxvi@users.noreply.github.com>
2020-06-12 18:10:46 +0200

    add logging to synchronizer

--- app/services/srf/api.rb
+++ app/services/srf/api.rb
@@ -1,11 +1,11 @@
 class Srf::Api
-  def initialize(date = Date.today.yesterday)
-    @from_time = date.at_beginning_of_day
-    @to_time = date.at_end_of_day
+  attr_reader :request
+
+  def initialize(date)
+    @request = Srf::Api::Request.new(date.at_beginning_of_day, date.at_end_of_day)
   end
 
   def call
-    request = Request.new(@from_time, @to_time)
     response = request.call
   end
 end
--- app/services/srf/synchronizer.rb
+++ app/services/srf/synchronizer.rb
@@ -1,11 +1,19 @@
 class Srf::Synchronizer
-  def initialize(date)
+  def initialize(date = Date.today.yesterday)
     @srf_api = Srf::Api.new(date)
     @errors = []
   end
 
   def synchronize!
-    srf_api_broadcasts.each(&:save!)
+    puts @srf_api.request.url
+    puts
+
+    srf_api_broadcasts.each do |srf_api_broadcast|
+      srf_api_broadcast.save!
+      puts summary(srf_api_broadcast)
+    rescue StandardError => e
+      puts summary(srf_api_broadcast, e)
+    end
   end
 
   def srf_api_broadcasts
@@ -16,5 +24,34 @@ class Srf::Synchronizer
 
   def api_response
     @api_response ||= @srf_api.call
+  end
+
+  def summary(srf_api_broadcast, error = nil)
+    [
+      ok_or_nok(error),
+      srf_api_broadcast.broadcasted_at,
+      artist_name_and_song_title(srf_api_broadcast)
+    ].join('  ')
+  end
+
+  def ok_or_nok(error)
+    return green('[OK] ') if error.nil?
+
+    red('[NOK]')
+  end
+
+  def artist_name_and_song_title(srf_api_broadcast)
+    [
+      srf_api_broadcast.srf_api_songlog_song.srf_api_songlog_artist.name,
+      srf_api_broadcast.srf_api_songlog_song.title
+    ].join(' - ')
+  end
+
+  def red(text)
+    "\e[31m#{text}\e[0m"
+  end
+
+  def green(text)
+    "\e[32m#{text}\e[0m"
   end
 end
