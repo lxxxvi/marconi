11d4529534c07148c47e98180efc31e54c5adc92
lxxxvi <lxxxvi@users.noreply.github.com>
2020-06-20 11:38:37 +0200

    implement epoch-columns into upsert statement

--- app/services/facts/calculator_base.rb
+++ app/services/facts/calculator_base.rb
@@ -30,18 +30,32 @@ class Facts::CalculatorBase
       static_data AS (
         SELECT '#{factable_type}'     AS factable_type
              , '#{fact_key}'          AS key
+             , #{epoch_year}          AS epoch_year
+             , #{epoch_week}          AS epoch_week
              , NOW()                  AS created_at
              , NOW()                  AS updated_at
       ),
       calculated_data AS (
         #{calculated_data_sql}
       )
-      INSERT INTO facts (station_id, factable_id, factable_type, key, value, created_at, updated_at)
+      INSERT INTO facts (
+          station_id
+        , factable_id
+        , factable_type
+        , key
+        , value
+        , epoch_year
+        , epoch_week
+        , created_at
+        , updated_at
+      )
       SELECT cd.station_id
            , cd.factable_id
            , sd.factable_type
            , sd.key
            , cd.value
+           , sd.epoch_year
+           , sd.epoch_week
            , sd.created_at
            , sd.updated_at
         FROM calculated_data cd
@@ -58,6 +72,16 @@ class Facts::CalculatorBase
 
   def factable_type
     raise 'implement in subclass'
+  end
+
+  # override in subclass if required
+  def epoch_year
+    Fact.column_defaults['epoch_year']
+  end
+
+  # override in subclass if required
+  def epoch_week
+    Fact.column_defaults['epoch_week']
   end
 
   # calculated_data_sql should be an SQL SELECT statement that returns three columns
