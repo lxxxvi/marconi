6698a60d256a0ac3480a393729e3d7da05e9e885
lxxxvi <lxxxvi@users.noreply.github.com>
2020-11-13 15:20:02 +0100

    API endpoint for predictions

--- app/models/prediction.rb
+++ app/models/prediction.rb
@@ -1,2 +1,10 @@
 class Prediction < ApplicationRecord
+  belongs_to :song
+
+  validates :reference_date, :score, presence: true
+
+  scope :for_date, ->(date) { where(reference_date: date) }
+  scope :ordered, -> { order(score: :desc) }
+
+  scope :with_song_and_artist, -> { includes(song: :artist).joins(song: :artist) }
 end
